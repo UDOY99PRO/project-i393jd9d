a7d1495ca6c3c23901d9c6ec09e1a81d12ef43aa
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2024-02-05 16:13:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+{
+  "name": "your-project-name",
+  "version": "1.0.0",
+  "description": "A brief description of your project",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "dependencies": {
+    "discord.js": "^14.14.1"
+  },
+  "author": "Your Name",
+  "license": "MIT"
+}
+  
