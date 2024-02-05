733d4192c0878f1518a0997a1f32053b0f103e17
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2024-02-05 16:15:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-{
-  "name": "your-project-name",
-  "version": "1.0.0",
-  "description": "A brief description of your project",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "dependencies": {
-    "discord.js": "^14.14.1"
-  },
-  "author": "Your Name",
-  "license": "MIT"
-}
-  
+var app = require("express")();
+app.get("/", async(req, res) => {
+res.send(true);
+});
+app.listen(3000);
