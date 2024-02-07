a6de8b5fc96ec23b9776242f5b010367c4635ada
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2024-02-07 21:42:24 +0530

    Update external bot global chat.js

--- external bot global chat.js
+++ external bot global chat.js
@@ -1,6 +1,13 @@
  var { GatewayIntentBits, Partials, Client, Events } = require("discord.js"); 
 const mongoose = require('mongoose');
 
+mongoose.connect(process.env.mongo_global, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 const client = new Client({
     fetchAllMembers: true,
