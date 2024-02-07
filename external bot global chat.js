7515092d72a06009230aeaee46c359cafd6b7276
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2024-02-07 20:49:06 +0530

    [render skip]

--- external bot global chat.js
+++ external bot global chat.js
@@ -1,1 +1,47 @@
+ var { GatewayIntentBits, Partials, Client, Events } = require("discord.js"); 
 
+const client = new Client({
+    fetchAllMembers: true,
+    restTimeOffset: 0,
+    failIfNotExists: false,
+    shards: "auto",
+    shardCount: 5,
+    allowedMentions: {
+      parse: ["roles", "users"],
+      repliedUser: true,
+    },
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER', 'MANAGE_MESSAGE', 'DIRECT_MESSAGE', Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction],
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildMessageReactions,
+      GatewayIntentBits.GuildPresences,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.DirectMessages,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.GuildVoiceStates
+    ]
+  });
+
+
+client.on('ready', async() => {
+  console.log(`Logged in as ${client.user.tag}`);
+  client.user.setPresence({
+    status: 'idle',
+  });
+  client.user.setActivity({
+    name: `🌍💬 Global Chat`, type: 0 
+  });
+
+client.application.commands.create({
+    name: 'help',
+    description: 'You will get help from this command',
+  });
+  
+   });
+
+
+
+
+
+client.login(process.env.GLOBAL_TOKEN);
