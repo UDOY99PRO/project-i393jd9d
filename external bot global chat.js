02e0be60fdf08d677c32e527844219ddd68ea10f
UDOY99PRO <147893083+UDOY99PRO@users.noreply.github.com>
2024-02-08 00:02:43 +0530

    Update external bot global chat.js

--- external bot global chat.js
+++ external bot global chat.js
@@ -50,7 +50,7 @@ await client.application.commands.create({
             description: 'Select the channel in which the global chat will be set.',
            type: 7,
             required: true,   
-            channelTypes: ['GUILD_TEXT'] // Specify channel type as GUILD_TEXT for text channels only
+            channelTypes: [0] // Specify channel type as GUILD_TEXT for text channels only
       }
     ]
 });
